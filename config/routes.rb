3a8c62c2d4bceea34b70fe7227881fe757bdeadf
Aidan McGuire <>
2021-05-23 15:52:42 -0600

    Remove old comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show] #do
-      #   resources :items, only: [:index]
-      # end
+      resources :merchants, only: [:index, :show] 
       namespace :merchants do
         get '/:merchant_id/items', to: 'search#index'
       end
 
       resources :items, only: [:index, :show]
-
       namespace :items do
         get '/:item_id/merchant', to: 'search#index'
       end
