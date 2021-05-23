51159667b69d92efe25d7a5a9354e18ad0053c57
Aidan McGuire <>
2021-05-23 14:14:17 -0600

    Add missing parenthesis

--- spec/models/merchants/merchant_items_spec.rb
+++ spec/models/merchants/merchant_items_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe '.merchant_items(merchant_id' do
+RSpec.describe '.merchant_items(merchant_id)' do
   it 'returns all items for the given merchant' do
     merchant = Merchant.create!(id: 1, name: 'merchant1')
     10.times do
