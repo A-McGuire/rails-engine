53693f3c6fd9755fea5a30a72b7cd9130f57d513
Aidan McGuire <>
2021-05-23 14:59:51 -0600

    Refactor get a merchants items out of items controller

--- app/controllers/Api/V1/items_controller.rb
+++ app/controllers/Api/V1/items_controller.rb
@@ -1,9 +1,5 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    if params[:merchant_id].present?
-      merchant_items = Merchant.merchant_items(params[:merchant_id])
-      return render json: ItemSerializer.new(merchant_items).serializable_hash
-    end
     items = ObjectsFacade.all_objects_helper(Item, params)
     render json: ItemSerializer.new(items).serializable_hash
   end
