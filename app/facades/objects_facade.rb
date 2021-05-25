fd6f5a3d741e3cf778f347aa2ec7bb8d73756b1d
Aidan McGuire <>
2021-05-25 13:24:42 -0600

    Add and remove comments

--- app/facades/objects_facade.rb
+++ app/facades/objects_facade.rb
@@ -1,6 +1,6 @@
 class ObjectsFacade
   class << self
-    def all_objects_helper(model, params) #TODO: test?
+    def all_objects_helper(model, params)
       return model.all_objects() if params[:page].nil? && params[:per_page].nil?
       return model.all_objects(params[:page].to_i, params[:per_page].to_i) if params[:page].present? && params[:per_page].present?
       return model.all_objects(1, params[:per_page].to_i) if params[:per_page].present?
