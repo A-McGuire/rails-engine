46f9170bcaf5bda26e602c3c737094e0b208b2af
Aidan McGuire <>
2021-05-26 17:03:22 -0600

    Clean up merchants controller

--- app/controllers/Api/V1/merchants_controller.rb
+++ app/controllers/Api/V1/merchants_controller.rb
@@ -13,21 +13,14 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def most_revenue
-    if valid_quantity?(params[:quantity])
-      merchants = Merchant.by_total_revenue(params[:quantity])
-      return render json: { status: "error", code: 400, message: "Quantity parameter required" } unless merchants
-      render json: MerchantNameRevenueSerializer.new(merchants).serializable_hash
-    else
-      render status: 400
-    end
+    return render status: 400 unless valid_quantity?(params[:quantity])
+    merchants = Merchant.by_total_revenue(params[:quantity])
+    render json: MerchantNameRevenueSerializer.new(merchants).serializable_hash
   end
 
   def most_items
-    if valid_quantity?(params[:quantity])
-      merchants = Merchant.most_items_sold(params[:quantity])
-      render json: ItemsSoldSerializer.new(merchants).serializable_hash
-    else
-      render status: 400
-    end
+    return render status: 400 unless valid_quantity?(params[:quantity])
+    merchants = Merchant.most_items_sold(params[:quantity])
+    render json: ItemsSoldSerializer.new(merchants).serializable_hash
   end
 end
