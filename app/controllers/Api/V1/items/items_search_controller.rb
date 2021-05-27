0855a561db4becea8f57d20a0e05c01d3133c5c3
Aidan McGuire <>
2021-05-26 21:50:32 -0600

    Refactor find all items search to accept all params, need refactor

--- app/controllers/Api/V1/items/items_search_controller.rb
+++ app/controllers/Api/V1/items/items_search_controller.rb
@@ -3,9 +3,19 @@ class Api::V1::Items::ItemsSearchController < ApplicationController
   include Validable
 
   def find_all
-    return render status: 400 unless valid_name?(params[:name])
-    items = Item.find_all(params[:name])
-    return render json: ItemSerializer.new(items).serializable_hash if items
-    render json: {data: []}, status: :ok
+    if params[:name]
+      return render status: 400 unless valid_name?(params[:name])
+      items = Item.find_all(params[:name])
+      return render json: ItemSerializer.new(items).serializable_hash if items
+      render json: {data: []}, status: :ok
+    elsif params[:min_price] && params[:max_price] || params[:max_price] || params[:min_price]
+      return render status: 400 unless valid_min_max(params[:min_price], params[:max_price])
+      params[:min_price] ||= 0
+      params[:max_price] ||= 9999999999
+      items = Item.find_by_min_max(params[:min_price], params[:max_price])
+      return render json: ItemSerializer.new(items).serializable_hash if items
+      render json: {data: []}, status: :ok
+    end
+    render status: 400
   end
 end
