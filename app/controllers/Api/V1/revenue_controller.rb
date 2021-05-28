d9e4188cbe65aecf06ea741ee5e7b33e60b9dae7
Aidan McGuire <>
2021-05-27 21:13:23 -0600

    Refactor revenue controller to handle sad path

--- app/controllers/Api/V1/revenue_controller.rb
+++ app/controllers/Api/V1/revenue_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::RevenueController < ApplicationController
+
+  include Validable
+
   def index
-    # return render status: 400 unless valid_quantity?(params[:quantity])
+    return render json: {status: 400, error: 'Invalid Parameters'}, status: 400 unless valid_start_end_dates?(params[:start], params[:end])
     revenue = Invoice.total_revenue_by_dates(params[:start], params[:end])
     render json: {data: {id: nil, type: 'revenue', attributes: {revenue: revenue}}}
   end
