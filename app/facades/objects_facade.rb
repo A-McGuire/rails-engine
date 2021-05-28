b5af69749351e362a89947b045f588483365553a
Aidan McGuire <>
2021-05-28 09:18:10 -0600

    Revove unecessary return clause

--- app/facades/objects_facade.rb
+++ app/facades/objects_facade.rb
@@ -4,7 +4,7 @@ class ObjectsFacade
       return model.all_objects() if params[:page].nil? && params[:per_page].nil?
       return model.all_objects(params[:page].to_i, params[:per_page].to_i) if params[:page].present? && params[:per_page].present?
       return model.all_objects(1, params[:per_page].to_i) if params[:per_page].present?
-      return model.all_objects(params[:page].to_i) if params[:page].present?
+      model.all_objects(params[:page].to_i) if params[:page].present?
     end
   end
 end
