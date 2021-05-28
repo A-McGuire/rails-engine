49eff79ea41f4c15451b2a010ddf4de6810d4171
Aidan McGuire <>
2021-05-28 09:30:50 -0600

    Refactor items search controller and integration test

--- app/controllers/Api/V1/items/items_search_controller.rb
+++ app/controllers/Api/V1/items/items_search_controller.rb
@@ -7,13 +7,11 @@ class Api::V1::Items::ItemsSearchController < ApplicationController
       return render status: 400 unless valid_name?(params[:name])
       items = Item.find_all(params[:name])
       return render json: ItemSerializer.new(items).serializable_hash if items
-      render json: {data: []}, status: :ok
     elsif params[:min_price] && params[:max_price] || params[:max_price] || params[:min_price]
       return render status: 400 unless valid_min_max?(params[:min_price], params[:max_price])
       params[:min_price] ||= 0; params[:max_price] ||= 9999999999
       items = Item.find_by_min_max(params[:min_price], params[:max_price])
       return render json: ItemSerializer.new(items).serializable_hash if items
-      render json: {data: []}, status: :ok
     end
     render status: 400
   end
--- spec/models/items/find_all_items_by_min_max_spec.rb
+++ spec/models/items/find_all_items_by_min_max_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'find all items by min max search' do
+  before :each do
+    FactoryBot.reload
+  end
+  
   it 'returns items within the params given' do
     item1 = create(:item, unit_price: 10)
     item2 = create(:item, unit_price: 20)
@@ -8,5 +12,23 @@ RSpec.describe 'find all items by min max search' do
     item4 = create(:item, unit_price: 40)
 
     expect(Item.find_by_min_max(11, 39)).to eq([item2, item3])
+  end
+
+  it 'returns no items if no items meet the search criteria' do
+    item1 = create(:item, unit_price: 10)
+    item2 = create(:item, unit_price: 20)
+    item3 = create(:item, unit_price: 30)
+    item4 = create(:item, unit_price: 40)
+
+    expect(Item.find_by_min_max(41, 50)).to eq([])
+  end
+
+  it 'returns all items if all items meet the search criteria' do
+    item1 = create(:item, unit_price: 10)
+    item2 = create(:item, unit_price: 20)
+    item3 = create(:item, unit_price: 30)
+    item4 = create(:item, unit_price: 40)
+
+    expect(Item.find_by_min_max(10, 40)).to eq([item1, item2, item3, item4])
   end
 end
